4b291cf27ff12d809a965cf8989b89b0a76151f5
LuYantao <yantao.lu@enerjoy.life>
2020-11-27 18:11:42 +0800

    add tcp header and length when sending data 已复查

--- Sources/Flower/MessageStream.swift
+++ Sources/Flower/MessageStream.swift
@@ -14,44 +14,35 @@ import Network
 import NetworkLinux
 #endif
 
-extension Connection
-{
-    public func readMessages(handler: @escaping (Message) -> Void)
-    {
-        self.receive(minimumIncompleteLength: 2, maximumLength: 2)
-        {
-            (maybeData, maybeContext, isComplete, maybeError) in
+extension Connection {
+    
+    public func readMessages(handler: @escaping (Message) -> Void) {
+        
+        self.receive(minimumIncompleteLength: 2, maximumLength: 2) { (maybeData, maybeContext, isComplete, maybeError) in
             
-            if let error = maybeError
-            {
+            if let error = maybeError {
                 print("Error when calling receive (message length) from readMessages: \(error)")
                 return
             }
             
-            guard let data = maybeData else
-            {
+            guard let data = maybeData else {
                 return
             }
             
             let length = Int(data.uint16!)
             
-            self.receive(minimumIncompleteLength: length, maximumLength: length, completion:
-            {
-                (maybeData, maybeContext, isComplete, maybeError) in
+            self.receive(minimumIncompleteLength: length, maximumLength: length, completion: { (maybeData, maybeContext, isComplete, maybeError) in
 
-                if let error = maybeError
-                {
+                if let error = maybeError {
                     print("Error when calling receive (message body) from readMessages: \(error)")
                     return
                 }
                 
-                guard let data = maybeData else
-                {
+                guard let data = maybeData else {
                     return
                 }
                 
-                guard let message = Message(data: data) else
-                {
+                guard let message = Message(data: data) else {
                     return
                 }
                 
@@ -61,29 +52,19 @@ extension Connection
         }
     }
 
-    public func writeMessage(message: Message, completion: @escaping (NWError?) -> Void)
-    {
-        let data = message.data
-        let length = UInt16(data.count)
-        let lengthData = length.data
+    public func writeMessage(message: Message, completion: @escaping (NWError?) -> Void) {
+        var data = Data()
+        data.append(UInt8(0))
+        data.append(UInt16(message.data.count).data)
+        data.append(message.data)
         
-        self.send(content: lengthData, contentContext: NWConnection.ContentContext.defaultMessage, isComplete: false, completion: NWConnection.SendCompletion.contentProcessed(
-            {
-                (maybeLengthError) in
+        self.send(content: data, contentContext: NWConnection.ContentContext.defaultMessage, isComplete: false, completion: NWConnection.SendCompletion.contentProcessed( { (maybeLengthError) in
                 
-                if let lengthError = maybeLengthError
-                {
+                if let lengthError = maybeLengthError {
                     print("Error sending length bytes. Error: \(lengthError)")
                     completion(lengthError)
                     return
                 }
-                
-                self.send(content: data, contentContext: NWConnection.ContentContext.defaultMessage, isComplete: false, completion: NWConnection.SendCompletion.contentProcessed(
-                {
-                    (maybeError) in
-                    
-                    completion(maybeError)
-            }))
         }))
     }
 }
